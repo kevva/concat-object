713843818db3e5f13910d74d8a2873f5f440f96a
Kevin Martensson <kevinmartensson@gmail.com>
2016-05-05 14:21:35 +0100

    Tweaks

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ import test from 'ava';
 import m from './';
 
 test(t => {
-	t.same(m({a: 'foo'}, {a: 'bar'}, {b: 'foo'}), {a: ['foo', 'bar'], b: ['foo']});
-	t.same(m({a: {b: 'foo'}}, {a: {b: 'bar'}}), {a: [{b: 'foo'}, {b: 'bar'}]});
-	t.same(m({a: ['a', 'b'], b: 'foo'}, {a: ['a', 'b'], b: 'bar'}), {a: [['a', 'b'], ['a', 'b']], b: ['foo', 'bar']});
-	t.same(m({a: [['a', 'b']]}, {a: [['a', 'b']]}, {a: [['a', 'b']]}), {a: [[['a', 'b']], [['a', 'b']], [['a', 'b']]]});
+	t.deepEqual(m({a: 'foo'}, {a: 'bar'}, {b: 'foo'}), {a: ['foo', 'bar'], b: ['foo']});
+	t.deepEqual(m({a: {b: 'foo'}}, {a: {b: 'bar'}}), {a: [{b: 'foo'}, {b: 'bar'}]});
+	t.deepEqual(m({a: ['a', 'b'], b: 'foo'}, {a: ['a', 'b'], b: 'bar'}), {a: [['a', 'b'], ['a', 'b']], b: ['foo', 'bar']});
+	t.deepEqual(m({a: [['a', 'b']]}, {a: [['a', 'b']]}, {a: [['a', 'b']]}), {a: [[['a', 'b']], [['a', 'b']], [['a', 'b']]]});
 });
